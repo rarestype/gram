d795c54a343519d1732e839209aec21b61922eef
Diana Ma <diana@rarestype.com>
2026-08-08 02:29:51 +0000

    binary digits

--- Sources/Grammar/Unicode/UnicodeDigit.Hex.swift
+++ Sources/Grammar/Unicode/UnicodeDigit.Hex.swift
@@ -1,14 +1,14 @@
 extension UnicodeDigit where Terminal: BinaryInteger {
     /// Matches a hexadecimal digit, without case-sensitivity,
     /// and returns its numeric value.
-    public enum Hex: DigitRule where Construction: BinaryInteger {
+    public enum Hex: DigitRule {
         @inlinable public static var radix: Construction { 16 }
         @inlinable public static func parse(terminal: Terminal) -> Construction? {
             switch terminal {
-            case 0x30 ... 0x39: return Construction.init(terminal - 0x30)
-            case 0x61 ... 0x66: return Construction.init(terminal +   10 - 0x61)
-            case 0x41 ... 0x46: return Construction.init(terminal +   10 - 0x41)
-            default:            return nil
+            case 0x30 ... 0x39: Construction.init(terminal - 0x30)
+            case 0x61 ... 0x66: Construction.init(terminal + 10 - 0x61)
+            case 0x41 ... 0x46: Construction.init(terminal + 10 - 0x41)
+            default: nil
             }
         }
     }
--- Sources/Grammar/Unicode/UnicodeDigit.Natural.swift
+++ Sources/Grammar/Unicode/UnicodeDigit.Natural.swift
@@ -1,7 +1,7 @@
 extension UnicodeDigit where Terminal: BinaryInteger {
     /// Matches a natural digit, `'1'` through `'9'` (`0x31 ... 0x39`),
     /// and returns its numeric value.
-    public enum Natural: TerminalRule where Construction: BinaryInteger {
+    public enum Natural: TerminalRule {
         @inlinable public static func parse(terminal: Terminal) -> Construction? {
             guard 0x31 ... 0x39 ~= terminal else {
                 return nil
